85ef5ce67d1fe8653ce5f87a986701b708d2eadc
warsou <marceline.lebourdais@gmail.com>
2019-10-10 23:59:07 +0200

    dernierpush

--- lib/01_mairiesChristmas.rb
+++ lib/01_mairiesChristmas.rb
@@ -3,25 +3,50 @@ require 'open-uri'
 
 
 
+
+
 #page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-def one_wmca
-page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/95/avernes.html"))
+
+def get_townhall_email(townhall_url)
+page = Nokogiri::HTML(open(townhall_url))
 
 #def get_townhall_email(page)
 
-mairies = page.xpath("//*/tr[4]/td[2]")
+mairie = page.xpath("//*/tr[4]/td[2]").text
+#mairie = mairie.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/i)
+#mairie = mairie.match("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b")
 
-mairies.each do |email|
-  puts email.text
+ville = page.xpath("/html/body/div/main/section[1]/div/div/div/h1").text
+ville = ville.split(' - ')[0]
+#mairie = mairie =~ /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]+)/
+pagereturn = []
+ h = Hash.new
+ h = { ville => mairie}
+ pagereturn.push(h)
+
+return pagereturn
 end
+puts get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html")
+
+
+def get_townhall_urls
+master_page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
+mairies = master_page.xpath('//* [@class="lientxt"]')
+pagereturn = []
+h = Hash.new
+mairies.each do |urlt|
+newUrl = urlt['href']
+newUrl = newUrl[1..-1]
+
+ville = urlt.text
+
+h = { ville => "https://www.annuaire-des-mairies.com"+newUrl}
+pagereturn.push(h)
+
+
 
 end
-one_wmca()
-
-def other_wmca
-page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/"))
-
+return pagereturn
 
 end
-
-#get_townhall_email("")
+ puts get_townhall_urls()
